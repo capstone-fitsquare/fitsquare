7fd058992e5f52e6cf55f1089ba65c2b5d4fb907
Will Shaw <wrcs505@gmail.com>
2018-01-26 12:17:15 -0500

    exercise and macro seeds

--- server/seed.js
+++ server/seed.js
@@ -42,12 +42,40 @@ const listItems = [
 
 const macroGoals = [
   {
-    name: 'bulking',
-    calories: 2500,
-    protein: 150,
+    name: 'Lean Bulking',
+    calories: 3000,
+    protein: 200,
     carbs: 300,
+    fat: 200
+  },
+  {
+    name: 'Hard Bulking',
+    calories: 4000,
+    protein: 250,
+    carbs: 325,
+    fat: 225
+  },
+  {
+    name: 'Cutting',
+    calories: 2000,
+    protein: 200,
+    carbs: 200,
     fat: 80
-  }
+  },
+  {
+    name: 'Maintenance',
+    calories: 2500,
+    protein: 200,
+    carbs: 250,
+    fat: 120
+  },
+  {
+    name: 'Keto',
+    calories: 2500,
+    protein: 250,
+    carbs: 25,
+    fat: 250
+  },
 ]
 
 const microGoals = [
@@ -67,11 +95,174 @@ const microGoals = [
 
 const exercises = [
   {
-    name: 'running'
+    name: 'Sprinting',
+    // category: Ambulatory,
+    // respiratory-type: Aerobic,
+    // resistance-type: Bodyweight
   },
   {
-    name: 'squats'
-  }
+    name: 'Jogging',
+    // category: Ambulatory,
+    // respiratory-type: Aerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Running',
+    // category: Ambulatory,
+    // respiratory-type: Aerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Walking',
+    // category: Ambulatory,
+    // respiratory-type: Aerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Barbell Low Bar Squat',
+    // category: Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Weighted
+  },
+  {
+    name: 'Deadlift',
+    // category: Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Weighted
+  },
+  {
+    name: 'Romanian Deadlift',
+    // category: Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Weighted
+  },
+  {
+    name: 'Overhead Press',
+    // category: Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Weighted
+  },
+  {
+    name: 'Bench Press'
+    // category: Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Weighted
+  },
+  {
+    name: 'Barbell Row',
+    // category: Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Weighted
+  },
+  {
+    name: 'Barbell Lunge',
+    // category: Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Weighted
+  },
+  {
+    name: 'Barbell Split Squat',
+    // category: Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Weighted
+  },
+  {
+    name: 'Weighted Dip',
+    // category:  Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Weighted
+  },
+  {
+    name: 'Pullup',
+    // category:  Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Chinup',
+    // category:  Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Basic Pushup',
+    // category:  Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Diamond Pushup',
+    // category:  Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'L-Sit',
+    // category:  Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Ski Squat',
+    // category:  Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Bodyweight Dip',
+    // category:  Strength/Hypertrophy,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Squat Jump',
+    // category:  Plyometric,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Split Squat Jump',
+    // category:  Plyometric,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Burpee',
+    // category:  Plyometric,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Depth Jump',
+    // category:  Plyometric,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Drop Jump',
+    // category:  Plyometric,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Slalom Jump',
+    // category:  Plyometric,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Standing Broad Jump',
+    // category:  Plyometric,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+  {
+    name: 'Bodyweight Lunge',
+    // category:  Plyometric,
+    // respiratory-type: Anaerobic,
+    // resistance-type: Bodyweight
+  },
+
 ]
 
 
