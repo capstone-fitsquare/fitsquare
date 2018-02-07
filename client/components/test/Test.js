14c8866c49be5e0d22a88d07f4fbf7feee83c4c8
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2018-02-06 22:14:00 -0500

    working on validation for twilio

--- client/components/test/Test.js
+++ client/components/test/Test.js
@@ -1,50 +1,52 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Grid, Segment, Form } from 'semantic-ui-react';
+import { Grid, Segment, Form, Message } from 'semantic-ui-react';
 import './test.css';
 
 class Test extends Component {
   constructor(props) {
     super(props);
-    this.state = { phonenumber: '', submittedphonenumber: '' };
+    this.state = { phonenumber: '', formSubmitted: false };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event, { phonenumber, value }) {
-    this.setState({ [phonenumber]: value });
+  handleChange(event) {
+    this.setState({ phonenumber: event.target.value });
   }
 
   handleSubmit() {
-    axios
-      .post('/api/send', {
-        body: this.state.phonenumber,
+    this.setState({ phonenumber: '', formSubmitted: true });
+    return axios
+      .post('/api/twilio', {
+        phonenumber: this.state.phonenumber,
       })
-      .then(res => res.json())
-      .then(res => console.log(res))
+      .then(resp => resp.data)
       .catch(error => console.log(error));
   }
 
   render() {
-    const { phonenumber } = this.state;
+    const { phonenumber, formSubmitted } = this.state;
+    console.log('phonenumber', phonenumber);
+
+    let message = null;
+    if (formSubmitted) {
+      message = <Message success header="Success!" content="Your grocery list has been texted to your phone" />;
+    }
 
     return (
       <Grid columns={1} stackable textAlign="center">
         <Grid.Column width={1} />
         <Grid.Column width={14}>
-          <Form onSubmit={this.handleSubmit}>
+          <Form success onSubmit={this.handleSubmit}>
             <Segment stacked>
               <Form.Group id="form-group" inline>
                 <label>Phone Number</label>
-                <Form.Input
-                  placeholder="+12223334444"
-                  phonenumber="phonenumber"
-                  value={phonenumber}
-                  onChange={this.handleChange}
-                />
+                <Form.Input onChange={this.handleChange} value={phonenumber} placeholder="+12223334444" />
               </Form.Group>
               <Form.Button id="form-group-button" content="Submit" />
+              {message}
             </Segment>
           </Form>
         </Grid.Column>
