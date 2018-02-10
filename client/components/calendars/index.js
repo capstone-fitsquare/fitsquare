900ef59dd3cefc3737b11e4eb4e66101f38b0558
Will Shaw <wrcs505@gmail.com>
2018-02-10 03:54:52 -0500

    user profile styling in progress

--- client/components/calendars/index.js
+++ client/components/calendars/index.js
@@ -7,6 +7,7 @@ import { Input, Button, Grid, Segment } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import moment from 'moment';
 import Select from 'react-select';
+import GridColumn from 'semantic-ui-react/dist/commonjs/collections/Grid/GridColumn';
 
 class Calendar extends Component {
   constructor() {
@@ -70,8 +71,8 @@ class Calendar extends Component {
         ]
       }
     }
-    console.log("meal log: ", meal)
-    // insertEventToCal(meal)
+    // console.log("meal log: ", meal)
+    insertEventToCal(meal)
   }
 
   render() {
@@ -93,66 +94,71 @@ class Calendar extends Component {
     }
   return (
     <div>
-      <div className="meal-schedule-form">
-        <Grid textAlign="center" style={{ height: '100%' }} verticalAlign="top">
-          <Grid.Column width={4}>
-          <h3>Select a Recipe</h3>
-          <Select
-            name="mealChange"
-            multi={true}
-            placeHolder={`When do you want to eat?`}
-            onChange={(recObj) => {
-              const selected = recObj.map(rec => rec)
+      <Grid className={'meal-schedule-form'}>
+      <GridColumn width={4}>
+      <h3>Select a Recipe</h3>
+      <Select
+        name="mealChange"
+        className="meal-selector"
+        multi={true}
+        placeHolder={`When do you want to eat?`}
+        onChange={(recObj) => {
+          const selected = recObj.map(rec => rec)
 
-              const mapCals = selected.map(rec => rec.value.calories).reduce((acc, val) => acc + val).toFixed();
-              const mapProtein = selected.map(rec => rec.value.protein).reduce((acc, val) => acc + val).toFixed();
-              const mapCarbs = selected.map(rec => rec.value.carbs).reduce((acc, val) => acc + val).toFixed();
-              const mapFat = selected.map(rec => rec.value.fat).reduce((acc, val) => acc + val).toFixed();
+          const mapCals = selected.map(rec => rec.value.calories).reduce((acc, val) => acc + val).toFixed();
+          const mapProtein = selected.map(rec => rec.value.protein).reduce((acc, val) => acc + val).toFixed();
+          const mapCarbs = selected.map(rec => rec.value.carbs).reduce((acc, val) => acc + val).toFixed();
+          const mapFat = selected.map(rec => rec.value.fat).reduce((acc, val) => acc + val).toFixed();
 
-              const newState = {
-                selectedRecipes: selected,
-                calories: mapCals,
-                protein: mapProtein,
-                carbs: mapCarbs,
-                fat: mapFat
+          const newState = {
+            selectedRecipes: selected,
+            calories: mapCals,
+            protein: mapProtein,
+            carbs: mapCarbs,
+            fat: mapFat
+          }
+          return this.setState(newState)
+        }}
+          options={nameMap}
+          value={ this.state.selectedRecipes }
+        />
+        </GridColumn>
+        <GridColumn width={4} >
+          <h3>Select a Time</h3>
+          <Datetime
+          input={false}
+          utc={true}
+          renderDay={ renderers.renderDay }
+          renderMonth={ renderers.renderMonth }
+          renderYear={ renderers.renderYear }
+          onChange={(mom) => {
+            const start = mom._d.toISOString().slice(0, -5);
+            const end = moment(mom._d).add(15, 'm')._d.toISOString().slice(0, -5);
+            this.setState({
+              start: {
+                "dateTime": start,
+                "timeZone": "America/New_York"
+              },
+              end: {
+                "dateTime": end,
+                "timeZone": "America/New_York"
               }
-              return this.setState(newState)
-            }}
-              options={nameMap}
-              value={ this.state.selectedRecipes }
-            />
-            <h3>Select a Time</h3>
-            <div className="Datetime-input">
-              <Datetime
-              input={false}
-              utc={true}
-              renderDay={ renderers.renderDay }
-              renderMonth={ renderers.renderMonth }
-              renderYear={ renderers.renderYear }
-              onChange={(mom) => {
-                const start = mom._d.toISOString().slice(0, -5);
-                const end = moment(mom._d).add(15, 'm')._d.toISOString().slice(0, -5);
-                this.setState({
-                  start: {
-                    "dateTime": start,
-                    "timeZone": "America/New_York"
-                  },
-                  end: {
-                    "dateTime": end,
-                    "timeZone": "America/New_York"
-                  }
-                })
-              }}
-              />
-            </div>
-            <form size="large" onSubmit={this.handleSubmit} name="calAuth">
-              <Segment stacked>
-                <Button type="Schedule">Schedule A Meal</Button>
-              </Segment>
-            </form>
-          </Grid.Column>
-        </Grid>
-      </div>
+            })
+          }}
+          />
+          <form size="large" onSubmit={this.handleSubmit} name="calAuth">
+            <Button type="Schedule" className={'meal-submit-btn'}>Schedule A Meal</Button>
+          </form>
+        </GridColumn>
+        <GridColumn width={6}>
+          <Segment>
+            <h3>{`Total Calories: ${this.state.calories}`}</h3>
+            <h3>{`Total Protein (g): ${this.state.protein}`}</h3>
+            <h3>{`Total Carbs (g): ${this.state.carbs}`}</h3>
+            <h3>{`Total Fat (g): ${this.state.fat}`}</h3>
+          </Segment>
+        </GridColumn>
+      </Grid>
     </div>
   )
   }
